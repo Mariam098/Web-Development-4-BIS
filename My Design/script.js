7ee002ae64078344a9d39f3e2f6b18a5dbb10811
Mariam098 <125475695+Mariam098@users.noreply.github.com>
2024-12-13 23:25:31 +0200

    Add files via upload

---
+++ My Design/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded" , function() {
+    let button_click = document.getElementById("AA");
+    button_click.addEventListener("click",click);
+    function click() {
+alert("purchase Red Rose Bouquet ?")
+    }
+}
+)
+document.addEventListener("DOMContentLoaded" , function() {
+    let button_click = document.getElementById("BB");
+    button_click.addEventListener("click",click);
+    function click() {
+alert("purchase Pink Rose Bouquet ?")
+    }
+}
+)
+document.addEventListener("DOMContentLoaded" , function() {
+    let button_click = document.getElementById("CC");
+    button_click.addEventListener("click",click);
+    function click() {
+alert("purchase White Tulip Bouquet ?")
+    }
+}
+)
+document.addEventListener("DOMContentLoaded" , function() {
+    let button_click = document.getElementById("DD");
+    button_click.addEventListener("click",click);
+    function click() {
+alert("purchase Sun Flower Bouquet ?")
+    }
+}
+)
+
